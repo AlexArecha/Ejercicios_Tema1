ca5ce9ad8d9ddaed9558b74d801334fe582f3112
Alex Arechabaleta <159237107+AlexArecha@users.noreply.github.com>
2024-02-08 12:04:21 +0000

    Cambios

---
+++ ejercicio_2.java
@@ -0,0 +1,11 @@
+//2. (5 minutos) Escribir un programa que muestre por consola los mayores
+//números enteros que se pueden representar mediante un char, short e int.
+
+public class ejercicio_2 {
+    public static void main (System[] args){
+    System.out.println(Character.MAX_VALUE);
+    System.out.println(Short.MAX_VALUE);
+    System.out.println(Integer.MAX_VALUE);
+
+    }
+}
